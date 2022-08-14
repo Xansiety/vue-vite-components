7083f6f8c3695f061a721e57abe0964aeb6b8cd1
xansiety <ferando543@outlook.com>
2022-08-14 16:11:10 -0500

    Paginacion de resultados con fetch

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,64 @@
  <script setup>
   import { ref } from 'vue' 
   import BlogPostComponent from './components/BlogPostComponent.vue'
-
-  const posts = ref([
-      { id: 1, title: "Post 01", body: "Descrión del post 01" },
-      { id: 2, title: "Post 02", body: "Descrión del post 02" },
-      { id: 3, title: "Post 03", body: "Descrión del post 03"  },
-      { id: 3, title: "Post 04" },
-  ]);
-
-
+  import ButtonsPaginate from './components/ButtonsPaginate.vue' 
+  
+  const posts = ref([])
   const favorito = ref('');
 
+  const itemsPorPagina = ref(10) 
+
+  // const currentPage = (number) => {
+  //   console.log(number);
+  //   itemsPorPagina.value = parseInt( number ); 
+  // }
+
+  //pagination
+  const pagination = ref({
+    start: 0,
+    end: itemsPorPagina.value 
+  })
+ 
   const cambiarFavorito = (title) => {
     favorito.value = title;
+  } 
+
+  const next = () => { 
+      pagination.value.start = pagination.value.start + itemsPorPagina.value;
+      pagination.value.end = pagination.value.end + itemsPorPagina.value;
   }
+
+  const previous = () => { 
+       pagination.value.start += -itemsPorPagina.value;
+       pagination.value.end += -itemsPorPagina.value;
+  }
+ 
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    posts.value = data
+  })
+  .catch(error => console.log(error)) 
  </script> 
  
 <template> 
 <div class="container"> 
   <div class="card mt-3 ">
-    <div class="card-title"><h2>Mis Post Favorito: {{favorito ? favorito : 'No hay ningún post fijado'}}</h2></div>
-    <div class="card-body">
+    <div class="card-title"><h2>Mi Post Favorito: {{favorito ? favorito : 'No hay ningún post fijado'}}</h2></div>
+    <ButtonsPaginate  @nextName="next" @prevName="previous" @currentPageName="currentPage" />
+    <div class="card-body"> 
        <!--  <BlogPostComponent v-for="post in posts" :key="post.id" :title="post.title" :id='post.id' :body="post.body" colorText="primary"
        @cambiarFavoritoNombre="cambiarFavorito" /> --> <!-- Enviamos un evento personalizado para que nuestro componente hijo, pueda afectar a muestro componente padre-->
       <!-- mandar eventos personalizados como props--> 
-          <BlogPostComponent v-for="post in posts" :key="post.id" :title="post.title" :id='post.id' :body="post.body" colorText="primary"
+          <BlogPostComponent v-for="post in posts.slice(pagination.start, pagination.end)" 
+          :key="post.id" 
+          :title="post.title" :id='post.id' :body="post.body" colorText="primary"
           :cambiarFavorito="cambiarFavorito"
-          @cambiarFavoritoNombre="cambiarFavorito" />  
+          @cambiarFavoritoNombre="cambiarFavorito" 
+          class="mb-3"/>  
     </div>
   </div> 
 </div>   
 </template> 
 
-<style lang='scss' scoped> 
-
-</style>
+ 
--- src/components/BlogPostComponent.vue
+++ src/components/BlogPostComponent.vue
@@ -24,29 +24,19 @@ defineProps({
 const emit = defineEmits(['cambiarFavoritoNombre'])
 
 //emit para poder ser accedido desde el setup
-//emit('cambiarFavorito', title)
-  
-
+//emit('cambiarFavorito', title) 
 </script>       
 
 <template> 
      <div class="card">
-        <div class="card-body d-flex justify-content-between ">
-            <div class="">
+        <div class="card-body "> 
                 <h5 class="card-title" :class="`text-${colorText}`"> {{id}} - {{title}}</h5>
-                <p> {{ body }} </p>
-            </div> 
-            <!-- Podemos acceder a $emit para poder capturar el evento personalizado que le estamos enviando a nuestro componente-->
-            <button class="btn btn-outline-success" @click="emit('cambiarFavoritoNombre', title)"> ⭐ </button>
-            
-            
-            <!-- Para trabajar con function props la forma de llamarlos cambia por esta forma -->
-            <!-- <button class="btn btn-outline-success" @click="cambiarFavorito(title)"> ⭐ </button> -->
+                <p> {{ body }} </p> 
+                <!-- Podemos acceder a $emit para poder capturar el evento personalizado que le estamos enviando a nuestro componente-->
+                <button class="btn btn-outline-success" @click="emit('cambiarFavoritoNombre', title)"> Favorito ⭐ </button>  
+                <!-- Para trabajar con function props la forma de llamarlos cambia por esta forma -->
+                <!-- <button class="btn btn-outline-success" @click="cambiarFavorito(title)"> ⭐ </button> -->
         </div>
      </div>
 </template> 
-
-
-<style lang='scss' scoped> 
-
-</style>
+ 
