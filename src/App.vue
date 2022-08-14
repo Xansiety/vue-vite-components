b586c1b98c905e941fb87251efc444b7a01d34e0
xansiety <ferando543@outlook.com>
2022-08-14 16:16:07 -0500

    Paginacion emit()

--- src/App.vue
+++ src/App.vue
@@ -42,23 +42,29 @@
  </script> 
  
 <template> 
-<div class="container"> 
-  <div class="card mt-3 ">
-    <div class="card-title"><h2>Mi Post Favorito: {{favorito ? favorito : 'No hay ningún post fijado'}}</h2></div>
-    <ButtonsPaginate  @nextName="next" @prevName="previous" @currentPageName="currentPage" />
-    <div class="card-body"> 
-       <!--  <BlogPostComponent v-for="post in posts" :key="post.id" :title="post.title" :id='post.id' :body="post.body" colorText="primary"
-       @cambiarFavoritoNombre="cambiarFavorito" /> --> <!-- Enviamos un evento personalizado para que nuestro componente hijo, pueda afectar a muestro componente padre-->
-      <!-- mandar eventos personalizados como props--> 
-          <BlogPostComponent v-for="post in posts.slice(pagination.start, pagination.end)" 
-          :key="post.id" 
-          :title="post.title" :id='post.id' :body="post.body" colorText="primary"
-          :cambiarFavorito="cambiarFavorito"
-          @cambiarFavoritoNombre="cambiarFavorito" 
-          class="mb-3"/>  
-    </div>
-  </div> 
-</div>   
+  <div class="container"> 
+    <div class="card mt-3 ">
+      <div class="card-title"><h2>Mi Post Favorito: {{favorito ? favorito : 'No hay ningún post fijado'}}</h2></div>
+
+      <ButtonsPaginate 
+       @nextName="next"
+       @prevName="previous" 
+       @currentPageName="currentPage"
+      />
+      
+      <div class="card-body"> 
+            <!--  <BlogPostComponent v-for="post in posts" :key="post.id" :title="post.title" :id='post.id' :body="post.body" colorText="primary"
+            @cambiarFavoritoNombre="cambiarFavorito" /> --> <!-- Enviamos un evento personalizado para que nuestro componente hijo, pueda afectar a muestro componente padre-->
+            <!-- mandar eventos personalizados como props--> 
+            <BlogPostComponent v-for="post in posts.slice(pagination.start, pagination.end)" 
+            :key="post.id" 
+            :title="post.title" :id='post.id' :body="post.body" colorText="primary"
+            :cambiarFavorito="cambiarFavorito"
+            @cambiarFavoritoNombre="cambiarFavorito" 
+            class="mb-3"/>  
+      </div>
+    </div> 
+  </div>   
 </template> 
 
  
