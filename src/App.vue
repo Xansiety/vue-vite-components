b08b45d56b6a165e265384e819901c2ef9eb557c
xansiety <ferando543@outlook.com>
2022-08-14 14:02:29 -0500

    Pasando props estaticos a nuestros componentes, y trabajando con ellos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
  <script setup>
   import ButtonComponent from './components/ButtonComponent.vue'
+  import BlogPostComponent from './components/BlogPostComponent.vue'
 
  </script> 
 
 <template> 
 <div class="container">
-   <h1> App counter </h1> 
+   <h1> App </h1> 
    <br>
    <!-- PascalCase: <ButtonComponent /> -->
    <ButtonComponent />
    <br>
-   <ButtonComponent />
+   <BlogPostComponent title="Post 01" :id='1' body="descipcion 01" colorText="primary"/>
+   <BlogPostComponent title="Post 02" :id='2' body="descipcion 02" colorText="secondary"/>
+   <BlogPostComponent title="Post 03" :id='3' body="descipcion 03" colorText="success"/>
+   <BlogPostComponent title="Post 04" :id='4' colorText="primary"/>
 </div> 
    <!-- or  -->
 
